150f278b9710c165fc6845a53857bc031856a2c0
jair alejandro bello perez <est.jair.bello@unimilitar.edu.co>
2026-04-24 09:59:04 -0500

    Implementación de drawPixel y algoritmo de Bresenham para líneas con manejo de octantes

--- script.js
+++ script.js
@@ -8,3 +8,40 @@
 //se optiene el elemento canva del archivo html por su id "canvas"
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+
+// Función de apoyo para dibujar un píxel individual
+function drawPixel(x, y, color = "#000000") {
+    ctx.fillStyle = color;  //define el color con el que se va a dibujar
+    ctx.fillRect(Math.floor(x), Math.floor(y), 1, 1);
+}
+
+// funcion bresenham 
+function bresenhamLine(x0, y0, x1, y1, color = "#000000") {
+    let dx = Math.abs(x1 - x0);
+    let dy = Math.abs(y1 - y0);
+
+    let sx = (x0 < x1) ? 1 : -1;
+    let sy = (y0 < y1) ? 1 : -1;
+
+    // Parametro de decision
+    let err = dx - dy;
+
+    while (true) {
+        drawPixel(x0, y0, color);
+
+        if (x0 === x1 && y0 === y1) break;
+
+        let e2 = 2 * err;
+
+        // ajusta el error
+        if (e2 > -dy) {
+            err -= dy;
+            x0 += sx;
+        }
+
+        if (e2 < dx) {
+            err += dx;
+            y0 += sy;
+        }
+    }
+}
