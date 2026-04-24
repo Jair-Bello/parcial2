1fef25134f4d90b1b47c7f38eb6b30138ddeac13
jair alejandro bello perez <est.jair.bello@unimilitar.edu.co>
2026-04-24 09:46:37 -0500

    Estructura base HTML5 y configuración del contexto 2D del canvas

---
+++ script.js
@@ -0,0 +1,10 @@
+/**
+ * Universidad - Facultad de Ingeniería
+ * Asignatura: Introducción a la Computación Gráfica
+ * * Estudiante: Jair Alejandro Bello Perez
+ * * Tarea: Implementar los algoritmos de rasterización manual.
+ */
+
+//se optiene el elemento canva del archivo html por su id "canvas"
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
